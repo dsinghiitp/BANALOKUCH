b05abf4938ce9a00c0a91bc90176cf84e5e55b07
ds <ds@Halo>
2012-06-27 13:20:13 +0530

    ye raha commit pehli baar

--- include/leftNavv.php
+++ include/leftNavv.php
@@ -2,8 +2,18 @@
 if (!defined('WEB_ROOT')) {
 	exit;
 }
+?>
+<div id="sidebar2">
+    <div class="sidebar_section">
+<?php
 $categoryList    = getCategoryList_dhiru($catId/*obtained from index.php*/);
                         foreach ($categoryList as $category) {
                         extract($category);
+                        ?>
+                        <a class="denapada3" <?php echo "href=\"$url\""?>><?php echo " $name"; ?></a>
+                        <?php
                         }
-?>
+
+                        ?>
+    </div>
+</div>
